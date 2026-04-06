d1fbb1b4212c73121cf2f5b3dd24c2f8b8162ac1
Atomic-Shadow7002 <atomic7002@gmail.com>
2026-04-06 09:59:18 +0530

    Intitialised Simple Ecom App

---
+++ simpleEcomApp/src/main/java/com/abhishek/simpleEcomApp/SimpleEcomAppApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.simpleEcomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEcomAppApplication.class, args);
+	}
+
+}
---
+++ simpleEcomApp/src/test/java/com/abhishek/simpleEcomApp/SimpleEcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.simpleEcomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
